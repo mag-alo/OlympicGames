aa44151ea6e774ef4dc207bb1f57080782857991
Magali Ducrot <magali.ducrot@laposte.net>
2025-04-11 16:00:04 +0200

    Init project

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -7,3 +7,35 @@ example of an olympic country:
     participations: []
 }
 */
+
+//MOI :
+// extrait d'olympic.json :
+// [
+//     {
+//       "id": 1,
+//       "country": "Italy",
+//       "participations": [
+//         {
+//           "id": 1,
+//           "year": 2012,
+//           "city": "Londres",
+//           "medalsCount": 28,
+//           "athleteCount": 372
+//         },
+//         {
+//           "id": 2,
+//           "year": 2016,
+//           "city": "Rio de Janeiro",
+//           "medalsCount": 28,
+//           "athleteCount": 375
+//         },
+//         {
+//           "id": 3,
+//           "year": 2020,
+//           "city": "Tokyo",
+//           "medalsCount": 40,
+//           "athleteCount": 381
+//         }
+//       ]
+//     },
+//\MOI
